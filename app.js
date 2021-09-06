a9b4698d672fbf060d8e19992f87d8ce1e6cabaf
Bumble <nick1092387456@gmail.com>
2021-09-06 11:12:00 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+//新增一個template叫做handlebars的引擎，並建立handlebars()來使用它
+app.engine('handlebars', handlebars())
+//設定view engine，使用handlebars
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
