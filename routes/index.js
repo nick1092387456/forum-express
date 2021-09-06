3fd7caef38c1203a96d7ee2a844c7385de02561f
Bumble <nick1092387456@gmail.com>
2021-09-06 22:21:18 +0800

    Add isAdmin to users table, Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,36 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 function router(app, passport) {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
   //如果使用者訪問首頁，就導向/restaurants頁面
-  app.get('/', (req, res) => {
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/restaurants')
   })
   //進入restaurants頁面後再轉交給restController輸出畫面
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin', (req, res) => {
+  app.get('/admin', authenticatedAdmin, (req, res) => {
     res.redirect('/admin/restaurants')
   })
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get(
+    '/admin/restaurants',
+    authenticatedAdmin,
+    adminController.getRestaurants
+  )
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
