5481378a83bd0f4b0f775f8c645cbffceea0b92f
Bumble <nick1092387456@gmail.com>
2021-09-06 20:20:34 +0800

    Install express-session connect-flash, Confirm user password and unique email

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const db = require('./models') //引入資料庫
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 //新增一個template叫做handlebars的引擎，並建立handlebars()來使用它，將參數帶入default main的樣板
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 //設定view engine，使用handlebars
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
