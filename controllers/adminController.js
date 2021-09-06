65d9801fe263b79153986135961430b102ccdc38
Bumble <nick1092387456@gmail.com>
2021-09-06 23:10:08 +0800

    Add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,13 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+
 //對應至router/index
 const adminController = {
+  //這裡決定要去view裡面抓哪個handlebars
   getRestaurants: (req, res) => {
-    //這裡決定要去view裡面抓哪個handlebars
-    return res.render('admin/restaurants')
+    return Restaurant.findAll({ raw: true }).then((restaurants) => {
+      return res.render('admin/restaurants', { restaurants: restaurants })
+    })
   },
 }
 
