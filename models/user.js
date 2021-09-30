16c2b36415ecca36fca43f6ddd24e5fb21783300
Bumble <nick1092387456@gmail.com>
2021-09-30 19:45:38 +0800

    feat: add profile.hbs

--- models/user.js
+++ models/user.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       isAdmin: DataTypes.BOOLEAN,
+      image: DataTypes.STRING,
     },
     {
       sequelize,
